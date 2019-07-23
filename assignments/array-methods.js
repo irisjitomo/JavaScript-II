13c81991c1c596c4a3cc5d7c650a2a441e14b3ea
irisjitomo <irisjitomo@eyahoo.com>
2019-07-23 18:13:12 -0400

    modified js files

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,55 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
-console.log(fullName);
-
+runners.forEach((runner) => {
+    fullName.push(runner.first_name + " " + runner.last_name)
+}) 
+console.log(fullName)
 // ==== Challenge 2: Use .map() ====
-// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
+// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. 
+// Convert each first name into all caps and log the result
 let allCaps = [];
-console.log(allCaps); 
+// console.log(allCaps); 
+runners.map((callback) => {
+    allCaps.push({'first_name': callback.first_name.toUpperCase()});
+})
+console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
-// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
+// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. 
+// Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
-console.log(largeShirts);
+// console.log(largeShirts);
+const L_Shirts = runners.filter((callback) => {
+    return callback.shirt_size === "L"
+})
+console.log(L_Shirts)
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+// console.log(ticketPriceTotal);
+const ticketTotalPrice = runners.reduce((total, price) => {
+    return total += price.donation;
+}, 0)
+console.log(ticketTotalPrice)
 
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  
+// Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+//use .filter()
+//the company wants to see a list of runners who donated more than $200. Make a list of the runners
+const bigBallers = runners.filter((callback) => {
+    return callback.donation >= 200
+})
+console.log(bigBallers);
 
 // Problem 2
+const upperCaseAllNames = runners.map((callback) => {
+    return {'first Name:': callback.first_name.toUpperCase(), 'last name': callback.last_name.toUpperCase() };
+})
+console.log(upperCaseAllNames)
 
 // Problem 3
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -29,23 +29,66 @@ function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 }
 
+function getLength(arr, cb) {
+  return cb(arr.length);
+}
+getLength(items, function(length){
+  console.log(length);
+})
+/*------------------------------------------------------------------------*/
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
+function last(arr, cb) {
+  return cb(arr[arr.length - 1])
+}
+last(items, function(lastItem){
+  console.log(lastItem);
+})
+/*------------------------------------------------------------------------*/
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+function add(x, y){
+  return (x + y);
+}
+function sumNums(x, y, cb){
+  return cb(x, y);
+}
+console.log (sumNums(1,2, add))
+/*------------------------------------------------------------------------*/
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
+function multiply(x, y){
+  return x * y;
+}
+function multiplyNums(x , y, cb){
+  return cb(x, y);
+}
+console.log(multiplyNums(2,7, multiply))
+/*------------------------------------------------------------------------*/
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+function contains(item, list, cb){
+  if (list.includes(item)){
+    return cb(true);
+  }
+  else {
+    return cb(false);
+  }
+}
 
+/*------------------------------------------------------------------------*/
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
